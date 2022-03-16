97e4562819f0e25e536ef47de41edd823ff1f85e
Etienne <januel.etienne@gmail.com>
2022-03-16 15:29:59 +0100

    yes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { QueryModule } from './modules/Query/query.module';
 
 @Module({
   imports: [QueryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
